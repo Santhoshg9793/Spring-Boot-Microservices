1f8920d3c59fa8e821f7f22c5136b124aaa6f357
Santhosh <santhoshg9793@gmail.com>
2019-12-11 22:19:07 +0530

    Initial commits
    Added Registry Service

---
+++ rolemanagement-registry-server/src/test/java/com/rolemanagement/microservices/RolemanagementRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolemanagement.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolemanagementRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
