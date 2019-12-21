d093a226255ea2c660dd453b653575d02d1ee044
Santhosh <santhoshg9793@gmail.com>
2019-12-21 16:29:14 +0530

    Added Falut tolerance mechanism

--- role-service/src/main/java/com/rolemanagement/microservices/RoleController.java
+++ role-service/src/main/java/com/rolemanagement/microservices/RoleController.java
@@ -6,16 +6,20 @@ package com.rolemanagement.microservices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * @author Santhosh
  *
  */
 @RestController
+@EnableHystrix
 public class RoleController {
 	private static Logger log = LoggerFactory.getLogger(RoleController.class);
 
@@ -24,9 +28,16 @@ public class RoleController {
 	private RoleRepository roleRepository;
 
 	@RequestMapping(value = "/role/{roleName}", method=RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "getRoleByNameFallback")
 	public Role getRoleByName(@PathVariable String roleName) {
 		log.info("Inside getRoleByName method");
-		return roleRepository.findByRoleName(roleName);
+		Role role=roleRepository.findByRoleName(roleName);
+		role.getRoleId();//Added to simulate fault tolerance incase if we pass data npt on DB
+		return role;
+	}
+	
+	public Role getRoleByNameFallback(@PathVariable String roleName) {
+		return Role.builder().roleId(0).roleName("Default Role").description("This is default system role").build();
 	}
 
 }
